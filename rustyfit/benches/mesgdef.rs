a2380ad190993bc0809bbc36a7439b51e128dc89
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2026-04-23 13:22:20 +0700

    bench: add benchmark for mesgdef

---
+++ rustyfit/benches/mesgdef.rs
@@ -0,0 +1,49 @@
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use rustyfit::{
+    profile::{ProfileType, mesgdef::Record, typedef::MesgNum},
+    proto::{Field, Message, Value},
+};
+
+pub fn bench_from(c: &mut Criterion) {
+    let mesg = Message {
+        num: MesgNum::RECORD,
+        fields: vec![
+            Field {
+                num: Record::DISTANCE,
+                profile_type: ProfileType::UINT32,
+                value: Value::Uint32(1000),
+                is_expanded: false,
+            },
+            Field {
+                num: 255,
+                ..Default::default()
+            },
+            Field {
+                num: 255,
+                ..Default::default()
+            },
+            Field {
+                num: 255,
+                ..Default::default()
+            },
+            Field {
+                num: 255,
+                ..Default::default()
+            },
+            Field {
+                num: 255,
+                ..Default::default()
+            },
+        ],
+        ..Default::default()
+    };
+
+    c.bench_function("mesgdef From<&Message>", |b| {
+        b.iter(|| {
+            let _ = Record::from(black_box(&mesg));
+        })
+    });
+}
+
+criterion_group!(benches, bench_from);
+criterion_main!(benches);
