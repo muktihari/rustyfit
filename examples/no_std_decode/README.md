d042393ad2b98facb7a199a6ad7b9c1070d309c8
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2026-05-19 19:49:40 +0700

    chore: add no_std example program (#48)

---
+++ examples/no_std_decode/README.md
@@ -0,0 +1,10 @@
+A sampe of #![no_std] program to decode a FIT file then print it into `stdout` on x86_64 linux machine.
+
+To run this program:
+```bash
+# Go to this directory because I put `.cargo/config.toml` local to this program only
+cd examples/no_std_decode
+
+# Run the program
+cargo run
+```
---
+++ examples/no_std_decode/src/main.rs
@@ -0,0 +1,106 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use alloc::format;
+use embedded_io_cursor::Cursor;
+use rustyfit::{
+    Decoder, DecoderEvent,
+    profile::{mesgdef, typedef},
+};
+use talc::{DefaultBinning, source::Claim, sync::TalcLock};
+
+#[global_allocator]
+static A: TalcLock<spinning_top::RawSpinlock, Claim, DefaultBinning> = TalcLock::new(unsafe {
+    const SIZE: usize = 48 * 1024; // 48 KB
+    static mut HEAP: [u8; SIZE] = [0; SIZE];
+    Claim::array(&raw mut HEAP)
+});
+
+const FIT_FILE: &[u8] = include_bytes!("sample.fit"); // Embed file
+
+macro_rules! printf {
+    ($($arg:tt)*) => {
+        let msg = format!($($arg)*);
+        let ret = sys_write(1, msg.as_ptr(), msg.len());
+        if ret != msg.len() {
+            sys_exit(1);
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
+fn _start() -> ! {
+    printf!("Hello, world!\n");
+
+    let mut cur = Cursor::new(FIT_FILE);
+    let mut dec = Decoder::new(cur.get_mut());
+
+    dec.decode_with(|e| match e {
+        DecoderEvent::Message(v) => {
+            if v.num == typedef::MesgNum::SESSION {
+                let ses = mesgdef::Session::from(v);
+                let sport = ses.sport;
+                let total_distance = ses.total_distance_scaled() / 1000.0;
+                printf!("> sport: {sport:}, total_distance: {total_distance:.2} km\n");
+            }
+        }
+        _ => {}
+    })
+    .unwrap();
+
+    printf!("Goodbye, world!\n");
+    sys_exit(0);
+
+    // # Output:
+    // Hello, world!
+    // > sport: running, total_distance: 9.40 km
+    // Goodbye, world!
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+// https://man7.org/linux/man-pages/man2/write.2.html
+// ```c
+// ssize_t write(int fd, const void buf[.count], size_t count);
+// ```
+//
+// ref: https://zenn.dev/zulinx86/articles/rust-nostd-101
+fn sys_write(fd: i32, buf: *const u8, count: usize) -> usize {
+    unsafe {
+        let ret: usize;
+        core::arch::asm!(
+            "syscall",
+            in("rax") 1,
+            in("rdi") fd,
+            in("rsi") buf,
+            in("rdx") count,
+            lateout("rax") ret,
+            out("rcx") _,
+            out("r11") _,
+        );
+        ret
+    }
+}
+
+// https://man7.org/linux/man-pages/man3/exit.3.html
+// ```c
+// [[noreturn]] void exit(int status);
+// ```
+//
+// ref: https://zenn.dev/zulinx86/articles/rust-nostd-101
+fn sys_exit(status: i32) -> ! {
+    unsafe {
+        core::arch::asm!(
+            "syscall",
+            in("rax") 60,
+            in("rdi") status,
+            options(noreturn)
+        );
+    }
+}
