9efb717a6a459c7f3c2030f0a40e9968e11011f7
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2026-05-21 14:48:18 +0700

    fitgen: generate mesgdef

--- rustyfit/src/profile/mesgdef/jump.rs
+++ rustyfit/src/profile/mesgdef/jump.rs
@@ -8,6 +8,7 @@
 
 use crate::profile::{ProfileType, typedef};
 use crate::proto::*;
+use crate::semconv;
 use alloc::vec::Vec;
 
 fn is_expanded(state: &[u8], num: u8) -> bool {
@@ -85,6 +86,16 @@ impl Jump {
             unknown_fields: Vec::new(),
             developer_fields: Vec::new(),
         }
+    }
+
+    /// Returns `position_lat` in degrees instead of semicircles. It returns invalid f64 when value is valid.
+    pub fn position_lat_degrees(&self) -> f64 {
+        semconv::to_degrees(self.position_lat)
+    }
+
+    /// Returns `position_long` in degrees instead of semicircles. It returns invalid f64 when value is valid.
+    pub fn position_long_degrees(&self) -> f64 {
+        semconv::to_degrees(self.position_long)
     }
 
     /// Returns `speed` in its scaled value. It returns invalid f64 when value is valid.
