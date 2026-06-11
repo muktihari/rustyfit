3e899ae1d2abd7186d0006f0e99c9e7c9d27861e
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2026-06-11 16:04:26 +0700

    fix: semconv returns incorrect value (#101)

--- rustyfit/src/semconv.rs
+++ rustyfit/src/semconv.rs
@@ -1,4 +1,4 @@
-const PI_RADIANS: f64 = (1 << 31) as f64;
+const PI_RADIANS: f64 = (1u32 << 31) as f64;
 const CONVERSION_FACTOR: f64 = 180.0 / PI_RADIANS;
 
 /// Converts semicircles to degrees. It returns f64 invalid when value is invalid.
@@ -15,4 +15,27 @@ pub fn to_semicircles(degrees: f64) -> i32 {
         return i32::MAX;
     }
     (degrees / CONVERSION_FACTOR) as i32
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::semconv;
+
+    #[test]
+    fn test_to_degrees() {
+        let lat: i32 = 424480360;
+        assert_eq!(semconv::to_degrees(lat), 35.579532757401466);
+
+        let long: i32 = -940295581;
+        assert_eq!(semconv::to_degrees(long), -78.81466512568295);
+    }
+
+    #[test]
+    fn test_to_semicircles() {
+        let lat: f64 = 35.579532757401466;
+        assert_eq!(semconv::to_semicircles(lat), 424480360);
+
+        let long: f64 = -78.81466512568295;
+        assert_eq!(semconv::to_semicircles(long), -940295581);
+    }
 }
